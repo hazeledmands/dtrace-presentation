b061c81861f53f110fdd3b87340cddaff1a527fc
Max Edmands <max@goodeggs.com>
2013-09-16 00:33:21 -0700

    http-server-request/response script gives useful information

--- 5.d
+++ 5.d
@@ -1,20 +1,28 @@
 /* Here's a script that outputs the exact time of every request/response cycle
-   as well as the time spent in GC. */
+   as well as the time spent in GC.
+   dtrace -s 5.d
+****/
 
 #pragma D option quiet
 
 BEGIN
 {
-  printf("%-22s %-20s %-8s %-16s %-16s %-16s\n",
-      "DIRECTION", "URL", "METHOD", "REMOTEADDRESS", "REMOTEPORT", "FD");
+  printf("Tracing your nodes....\n")
 }
 
 node*:::http-server-request
 {
+  /* thread-local variable: */
+  self->ts = timestamp;
 }
 
 node*:::http-server-response
 {
-  printf("%-22s %-20s %-8s %-16s %-16d %-16d\n",
-      probename, " ", " ", args[0]->remoteAddress, args[1]->remotePort, args[1]->fd);
+  this->time = timestamp - self->ts;
+  self->ts = 0;
+
+  printf("%s spent %d nsecs building an http-server-response\n", execname, this->time);
+
+  @avg_time[strjoin("Average time for ", execname)] = avg(this->time);
+  @quant_time[strjoin("Quantized time distribution for ", execname)] = quantize(this->time);
 }
